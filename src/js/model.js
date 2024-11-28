6c1171f3e04814abd2bee814d01f85fce8659ed8
gdevgproject <gdev.gproject@gmail.com>
2024-11-28 13:07:25 +0700

    v2.0.0 refactor: Refactoring for MVC and use fractional npm

---
+++ src/js/model.js
@@ -0,0 +1,32 @@
+export const state = {
+  recipe: {},
+};
+
+export const loadRecipe = async function (id) {
+  try {
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+    );
+
+    if (!res.ok) throw Error(`${res.status}`);
+
+    const data = await res.json();
+
+    console.log(res, data);
+
+    const { recipe } = data.data;
+
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+  } catch (err) {
+    alert(err);
+  }
+};
