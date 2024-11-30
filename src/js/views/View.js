44b48a8eca22d1c90453139527460420fb3703de
gdevgproject <gdev.gproject@gmail.com>
2024-11-30 15:10:43 +0700

    v3.4.1 build with parcel v2

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -9,7 +9,7 @@ export default class View {
    * @param {boolean} [render=true] If false, create markup string instead of rendering to the DOM
    * @returns {undefined | string} A markup string is returned if render=false
    * @this {Object} View instance
-   * @author Jonas Schmedtmann
+   * @author Vu Duc Minh
    * @todo Finish implementation
    */
   render(data, render = true) {
