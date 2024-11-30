9429595211a19feb5019b253b8c4f261f7bdb751
gdevgproject <gdev.gproject@gmail.com>
2024-11-30 16:06:25 +0700

    v3.4.2 fix: dependencies version and rebuild

--- src/js/controller.js
+++ src/js/controller.js
@@ -7,6 +7,10 @@ import paginationView from './views/paginationView.js';
 import bookmarksView from './views/bookmarksView.js';
 import addRecipeView from './views/addRecipeView.js';
 
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import { async } from 'regenerator-runtime';
+
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,5 +1,6 @@
 import View from './View.js';
 
+// import icons from '../img/icons.svg'; // Parcel 1
 import icons from 'url:../../img/icons.svg'; // Parcel 2
 import { Fraction } from 'fractional';
 
